acaadc335136d93f4ec53d2f040b523b44c4d8f0
caliche17 <cavelasquez17@gmail.com>
2023-01-21 12:07:47 -0500

    get jokes request

--- app/controllers/jokes_controller.rb
+++ app/controllers/jokes_controller.rb
@@ -1,14 +1,9 @@
+require 'uri'
+require 'net/http'
 class JokesController < ApplicationController
-  require 'uri'
-  require 'net/http'
-
   def get_joke
     url = URI('https://api.chucknorris.io/jokes/random')
-    https = Net::HTTP.new(url.host, url.port)
-    request = Net::HTTP::Get.new(url)
-    request['Content-Type'] = 'application/x-www-form-urlencoded'
-    byebug
-    response = https.request(request)
-    parsed_response = JSON.parse(response.read_body)
+    response = Net::HTTP.get(url)
+    JSON.parse(response)['value']
   end
 end
